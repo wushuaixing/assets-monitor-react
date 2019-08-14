b29f47bdaa618023dca7710a6f92f090ea1965f6
metotna <me-totna@foxmail.com>
2019-08-14 15:40:29 +0800

    -feat 机构管理基本路由

--- src/views/organization/index.jsx
+++ src/views/organization/index.jsx
@@ -1,20 +1,51 @@
 import React from 'react';
-// import Router from '@/utils/Router';
+import { navigate } from '@reach/router';
+
+import Router from '@/utils/Router';
 import Tabs from '@/common/tabs';
+
+import PushManage from './push-manage';
+import UserList from './user-list';
+import OperateLog from './operate-log';
 
 const source = [
 	{
-		id: 1, name: '推送设置', url: '', number: 0, dot: false,
+		id: 1,
+		name: '推送设置',
+		url: '/organization',
+		number: 0,
+		dot: false,
+		components: PushManage,
 	},
 	{
-		id: 2, name: '机构管理', url: '', number: 0, dot: false,
+		id: 2,
+		name: '账号列表',
+		url: '/organization/user',
+		number: 0,
+		dot: false,
+		components: UserList,
 	},
 ];
-const monitorBase = () => (
+const BusinessBase = () => (
 	<React.Fragment>
-		<Tabs rightRender={() => <span>eeee</span>} onChange={res => console.log(res)} source={source} />
-		<div>这是监控信息模块</div>
+		<Tabs
+			onChange={res => navigate(res.url)}
+			source={source}
+		/>
+		<div className="yc-business yc-page-content">
+			<Router>
+				{
+					source.map(Item => <Item.components path={`${Item.url}/*`} />)
+				}
+			</Router>
+		</div>
 	</React.Fragment>
 
 );
-export default monitorBase;
+const BusinessRouter = () => (
+	<Router>
+		<BusinessBase path="/*" />
+		<OperateLog path="/organization/operate/log/*" />
+	</Router>
+);
+export default BusinessRouter;
---
+++ src/views/organization/push-manage/index.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <div>推送设置</div>;
