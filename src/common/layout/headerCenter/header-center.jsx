a2c61875db46429ad8a5c23cf1b48e055cfc3993
撸撸 <lulu@luludeMacBook-Air.local>
2019-08-20 08:47:07 +0800

    -feat 消息中心

---
+++ src/common/layout/headerCenter/header-center.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import { Tree, Input } from 'antd';
+import './style.scss';
+
+const { TreeNode } = Tree;
+export default class HeaderMessage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		};
+	}
+
+	inputValue= (e) => {
+		const { value } = e.target;
+		console.log(value);
+
+		// const arr = flat(dataList) && flat(dataList).filter(item => item !== undefined);
+		// this.setState({
+		// 	selectList: this.filterByName(arr, value),
+		// 	searchValue: value,
+		// });
+	}
+
+	render() {
+		const gData = [{
+			key: 1,
+			name: '崔金鑫测试机构121（汇总）',
+			age: 32,
+			num: 122,
+			all: 1233,
+			totalMoney: '106,202.00',
+			children: [{
+				key: 11,
+				name: '崔金鑫测试机构12崔金鑫测试机构1211崔金鑫测试机构121111',
+				age: 33,
+				num: 122,
+				all: 1233,
+				totalMoney: '106,202.00',
+				url: '#/monitor',
+			}, {
+				key: 12,
+				name: '崔金鑫测试机构121',
+				age: 33,
+				num: 122,
+				all: 1233,
+				totalMoney: '106,202.00',
+				children: [{
+					key: 121,
+					name: '崔金鑫测试机构1211',
+					age: 33,
+					num: 122,
+					all: 1233,
+					totalMoney: '106,202.00',
+				}, {
+					key: 121,
+					name: '崔金鑫测试机构1211',
+					age: 33,
+					num: 122,
+					all: 1233,
+					totalMoney: '106,202.00',
+				}, {
+					key: 12,
+					name: '崔金鑫测试机构121',
+					age: 33,
+					num: 122,
+					all: 1233,
+					totalMoney: '106,202.00',
+					children: [{
+						key: 121,
+						name: '崔金鑫测试机构1211',
+						age: 33,
+						num: 122,
+						all: 1233,
+						totalMoney: '106,202.00',
+					}, {
+						key: 121,
+						name: '崔金鑫测试机构1211',
+						age: 33,
+						num: 122,
+						all: 1233,
+						totalMoney: '106,202.00',
+					}],
+				}, {
+					key: 12,
+					name: '崔金鑫测试机构121',
+					age: 33,
+					num: 122,
+					all: 1233,
+					totalMoney: '106,202.00',
+					children: [{
+						key: 121,
+						name: '崔金鑫测试机构1211',
+						age: 33,
+						num: 122,
+						all: 1233,
+						totalMoney: '106,202.00',
+					}, {
+						key: 121,
+						name: '崔金鑫测试机构1211',
+						age: 33,
+						num: 122,
+						all: 1233,
+						totalMoney: '106,202.00',
+					}],
+				}],
+			}, {
+				key: 13,
+				name: '崔金鑫测试机构1212',
+				num: 33,
+				all: 1233,
+				totalMoney: '106,202.00',
+				children: [{
+					key: 131,
+					name: '崔金鑫测试机构21',
+					num: 33,
+					all: 1233,
+					totalMoney: '106,202.00',
+					children: [{
+						key: 1311,
+						name: '崔金鑫测试机构12122',
+						num: 33,
+						all: 1233,
+						totalMoney: '106,202.00',
+					}, {
+						key: 1312,
+						name: '崔金鑫测试机构1213',
+						num: 33,
+						all: 1233,
+						totalMoney: '106,202.00',
+					}],
+				}],
+			}],
+		}];
+
+		const loop = tree => tree.map((item) => {
+			if (item.children) {
+				return (
+					<TreeNode key={item.key} title={item.name}>
+						{loop(item.children)}
+					</TreeNode>
+				);
+			}
+			return <TreeNode key={item.key} title={item.name} />;
+		});
+		return (
+			<div className="yc-header-center">
+				<div className="user-panel-item user-panel-msg clearfix">
+					<div className="g-left">
+						<div className="user-panel-item-text">您好，崔九九 </div>
+						<div className="user-panel-item-text">当前机构：崔金鑫测试机构11 </div>
+					</div>
+					<div className="g-right user-panel-right">
+						<div>
+							<a className="text-prompt user-panel-login" data-toggle="modal" data-target="#Modalupdate">修改密码</a>
+							<a className="user-panel-login" href="/User/Index/logout">退出登录</a>
+						</div>
+					</div>
+				</div>
+				<div className="yc-search-container">
+					<Input
+						className="yc-group-input"
+						onInput={e => this.inputValue(e)}
+						placeholder="请输入机构名称..."
+						size="large"
+					/>
+					{/* <Button onClick={() => this.btnSearch(searchValue || '')} className="yc-group-button">搜索</Button> */}
+					<div>
+						<Tree
+						// defaultExpandedKeys={this.state.expandedKeys}
+							draggable
+							onDragEnter={this.onDragEnter}
+							onDrop={this.onDrop}
+						>
+							{loop(gData)}
+						</Tree>
+					</div>
+
+				</div>
+			</div>
+		);
+	}
+}
--- src/views/business/business-detail/index.jsx
+++ src/views/business/business-detail/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { navigate } from '@reach/router';
 import {
-	Breadcrumb, Button, Table, Pagination,
+	Breadcrumb, Button, Table, Pagination, Input, Form,
 } from 'antd';
-
 import './style.scss';
+
+const createForm = Form.create;
 
 const columns = [{
 	title: '资产信息',
@@ -31,14 +32,54 @@ for (let i = 0; i < 12; i += 1) {
 	});
 }
 
-export default class DebtorDetail extends React.Component {
+
+class DebtorDetail extends React.Component {
 	constructor(props) {
 		super(props);
+		this.columns1 = [{
+			title: '相关人名称',
+			dataIndex: 'a',
+			width: 375,
+			render(text) {
+				return <a href="#">{text}</a>;
+			},
+		}, {
+			title: '身份证号/统一社会信用代码',
+			dataIndex: 'b',
+			width: 375,
+		}, {
+			title: '角色',
+			dataIndex: 'c',
+		}];
 		this.state = {
 			totals: 0,
 			current: 1, // 当前页
 			pageSize: 10, // 默认展示条数
+			edit: false,
+			detail: {
+				caseNumber: 11123,
+				obligorName: '占三',
+				obligorNumber: 'TEST22335662',
+				orgName: 'Test机构',
+				guarantee: '担保',
+			},
 		};
+	}
+
+	handleCancal = () => {
+		this.setState({
+			edit: false,
+		});
+	}
+
+	handleEdit = (edit) => {
+		if (edit === false) {
+			this.setState({
+				edit: !edit,
+			});
+		} else {
+			console.log(2);
+		}
 	}
 
 	// page翻页
@@ -58,39 +99,145 @@ export default class DebtorDetail extends React.Component {
 		// this.getData(params);
 	}
 
+
 	render() {
 		const {
-			totals, current,
+			totals, current, edit, detail,
 		} = this.state;
+		const { form } = this.props; // 会提示props is not defined
+		const { getFieldProps } = form;
 		return (
-			<div className="yc-debtor-wrapper">
+			<div className="yc-business-wrapper">
 				<div className="yc-content-breadcrumb">
 					<Breadcrumb>
 						<Breadcrumb.Item><p style={{ fontSize: 14 }} className="click-p" onClick={() => navigate('/business/debtor')}>业务视图</p></Breadcrumb.Item>
 						<Breadcrumb.Item><span style={{ 'font-weight': 100 }}>业务详情</span></Breadcrumb.Item>
 					</Breadcrumb>
-				</div>
-				<div className="yc-item-ob">
-					<div className="yc-item-icon" />
-					<div className="yc-search-content">
-						<span className="yc-item-title">p</span>
-						<div className="search-item-text">
-							<span className="search-item-text-header">身份证号/统一社会信用代码：</span>
-							<span className="search-item-text-msg">2222</span>
-						</div>
-						<div className="search-item-text">
-							<span className="search-item-text-header">曾用名：</span>
-							<span className="search-item-text-msg">－</span>
-						</div>
-					</div>
 					<div className="yc-search-right">
-						<Button className="yc-btn">
-							<span className="yc-icon-export" />
-                            下载
+						<Button onClick={() => this.handleEdit(edit)} className="yc-btn">
+							{edit ? '保存' : '编辑'}
 						</Button>
+						{edit === false
+							? (
+								<React.Fragment>
+									<Button className="yc-btn">
+                                        变更记录
+									</Button>
+									<Button className="yc-btn">
+										<span className="yc-icon-export" />
+                                        下载
+									</Button>
+								</React.Fragment>
+							) : (
+								<Button onClick={() => this.handleCancal()} className="yc-btn">
+									{/* {!edit && <span className="yc-icon-export" />} */}
+                                    取消
+								</Button>
+							) }
 					</div>
 				</div>
-				<div className="yc-debtor-table">
+
+				<div className="yc-business-ob">
+					<div className="yc-module-title">基本信息</div>
+					{edit === false ? (
+						<div style={{ padding: '0 35px' }}>
+							<div className="yc-form-group">
+								<div className="yc-basic-msg-title">业务编号:</div>
+								<div className="yc-basic-msg-inner">{detail.caseNumber}</div>
+							</div>
+							<div className="yc-form-group">
+								<div className="yc-basic-msg-title">借款人名称:</div>
+								<div className="yc-basic-msg-inner">{detail.obligorName}</div>
+							</div>
+							<div className="yc-form-group">
+								<div className="yc-basic-msg-title">身份证号/统一社会信用代码:</div>
+								<div className="yc-basic-msg-inner">{detail.obligorNumber}</div>
+							</div>
+							<div className="yc-form-group">
+								<div className="yc-basic-msg-title">机构名称:</div>
+								<div className="yc-basic-msg-inner">{detail.orgName}</div>
+							</div>
+							<div className="yc-form-group">
+								<div className="yc-basic-msg-title">担保方式:</div>
+								<div className="yc-basic-msg-inner">{detail.guarantee}</div>
+							</div>
+						</div>
+					) : (
+						<div style={{ padding: '0 35px' }}>
+							<div className="yc-from-container">
+								<span className="yc-from-lable1">业务编号:</span>
+								<Input
+									{...getFieldProps('caseNumber', {
+										initialValue: detail.caseNumber,
+										// rules: [
+										// 	{ required: true, whitespace: true, message: '请填写密码' },
+										// ],
+									})}
+									className="yc-from-input"
+								/>
+							</div>
+							<div className="yc-from-container">
+								<span className="yc-from-lable2">
+									<span className="yc-red">*</span>
+									借款人名称:
+								</span>
+								<Input
+									{...getFieldProps('obligorName', {
+										initialValue: detail.obligorName,
+										// rules: [
+										// 	{ required: true, whitespace: true, message: '请填写密码' },
+										// ],
+									})}
+									className="yc-from-input"
+								/>
+							</div>
+							<div className="yc-from-container">
+								<span className="yc-from-lable1">机构名称:</span>
+								<Input
+									{...getFieldProps('orgName', {
+										initialValue: detail.orgName,
+										// rules: [
+										// 	{ required: true, whitespace: true, message: '请填写密码' },
+										// ],
+									})}
+									className="yc-from-input"
+								/>
+							</div>
+							<div className="yc-from-container">
+								<span className="yc-from-lable2">身份证号/统一社会信用代码:</span>
+								<Input
+									{...getFieldProps('obligorNumber', {
+										initialValue: detail.obligorNumber,
+										// rules: [
+										// 	{ required: true, whitespace: true, message: '请填写密码' },
+										// ],
+									})}
+									className="yc-from-input"
+								/>
+							</div>
+						</div>
+					)}
+				</div>
+				<div className="yc-business-table">
+					<div className="yc-table-header">
+						<div className="table-header-left">
+                            业务相关人列表
+						</div>
+					</div>
+					{/* <Table
+						columns={this.columns1}
+						dataSource={edit === false ? defaultData : cellData}
+						style={{ width: '100%' }}
+						pagination={false}
+						onRowClick={(record) => {
+							if (!record.children) {
+								const w = window.open('about:blank');
+								w.location.href = '#/monitor';
+							}
+						}}
+					/> */}
+				</div>
+				<div className="yc-business-table">
 					<div className="yc-table-header">
 						<div className="table-header-left">
 							资产拍卖
@@ -129,3 +276,4 @@ export default class DebtorDetail extends React.Component {
 		);
 	}
 }
+export default createForm()(DebtorDetail);
