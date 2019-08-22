e5234d6ffd5d9c4e08ec7bba0c0c754790ce5a00
撸撸 <lulu@luludeMacBook-Air.local>
2019-08-22 18:30:31 +0800

    -feat 业务编辑

--- src/views/business/business-detail/edit.jsx
+++ src/views/business/business-detail/edit.jsx
@@ -30,65 +30,66 @@ class DebtorDetail extends React.Component {
 		<table className="table" style={{ marginBottom: 0 }}>
 			<thead className="ant-table-thead">
 				<tr>
-				<th style={{ width: 400 }}>相关人名称</th>
-				<th style={{ width: 400 }}>身份证号/统一社会信用代码</th>
-				<th style={{ width: 250 }}>角色</th>
-				<th style={{ width: 110 }}>操作</th>
-   </tr>
+					<th style={{ width: 400 }}>相关人名称</th>
+					<th style={{ width: 400 }}>身份证号/统一社会信用代码</th>
+					<th style={{ width: 250 }}>角色</th>
+					<th style={{ width: 110 }}>操作</th>
+				</tr>
 			</thead>
 			<tbody className="ant-table-tbody">
 				{data && data.map(item => (
-				<tr>
-				<td>
-				<Input
-				size="large"
-				placeholder="请输入相关人名称"
-				{...getFieldProps('obligorName', {
-                	initialValue: item && item.obligorName,
-                	// rules: [
-                	// 	{ required: true, whitespace: true, message: '请填写密码' },
-                	// ],
-			})}
-				className="yc-from-input"
-			/>
-			</td>
-				<td>
-				<Input
-				size="large"
-				placeholder="请输入身份证号/统一社会信用代码"
-				{...getFieldProps('obligorNumber', {
-                	initialValue: item && item.obligorNumber,
-                	// rules: [
-                	// 	{ required: true, whitespace: true, message: '请填写密码' },
-                	// ],
-			})}
-				className="yc-from-input"
-			/>
-			</td>
-				<td>
-				<Select
-				style={{ width: 200 }}
-				placeholder="请选择人员"
-				size="large"
-				{...getFieldProps('role', {
-                	initialValue: item && item.role,
-                	// rules: [
-                	// 	{ required: true, whitespace: true, message: '请填写密码' },
-                	// ],
-			})}
-			>
-				<Option value={2}>担保人</Option>
-				<Option value={4}>共同借款人</Option>
-   </Select>
-   </td>
-				<td>
-				<Button type="ghost">
-				{/* {!edit && <span className="yc-icon-export" />} */}
-                                            删除
-   </Button>
-   </td>
-   </tr>
-			))}
+					<tr>
+						<td>
+							<Input
+								size="large"
+								placeholder="请输入相关人名称"
+								{...getFieldProps(`obligorName${item.id}`, {
+									id: item.id,
+									initialValue: item && item.obligorName,
+									// rules: [
+									// 	{ required: true, whitespace: true, message: '请填写密码' },
+									// ],
+								})}
+								className="yc-from-input"
+							/>
+						</td>
+						<td>
+							<Input
+								size="large"
+								placeholder="请输入身份证号/统一社会信用代码"
+								{...getFieldProps(`obligorNumber${item.id}`, {
+									initialValue: item && item.obligorNumber,
+									// rules: [
+									// 	{ required: true, whitespace: true, message: '请填写密码' },
+									// ],
+								})}
+								className="yc-from-input"
+							/>
+						</td>
+						<td>
+							<Select
+								style={{ width: 200 }}
+								placeholder="请选择人员"
+								size="large"
+								{...getFieldProps(`role${item.id}`, {
+									initialValue: item && item.role,
+									// rules: [
+									// 	{ required: true, whitespace: true, message: '请填写密码' },
+									// ],
+								})}
+							>
+								<Option value={2}>担保人</Option>
+								<Option value={4}>共同借款人</Option>
+							</Select>
+						</td>
+						<td>
+							<Button type="ghost">
+								{/* {!edit && <span className="yc-icon-export" />} */}
+                                    删除
+							</Button>
+						</td>
+					</tr>
+				))}
 
 			</tbody>
 		</table>
