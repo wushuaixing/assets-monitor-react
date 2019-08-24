41834cbdbc64d77798d39f2ebe7baea73bbd37bd
metotna <me-totna@foxmail.com>
2019-08-24 15:37:01 +0800

    -fix 测试网地址

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -3,8 +3,9 @@ import service from '../service';
 // export const baseUrl = 'http://172.18.255.121:8080';
 // export const baseUrl = 'http://172.18.255.74:8080'; // 赛神
 // export const baseUrl = 'http://172.18.255.32:8080'; // 胡歆
-export const baseUrl = 'http://172.18.255.141'; // 本机地址
+// export const baseUrl = 'http://172.18.255.141'; // 本机地址
 // export const baseUrl = 'http://172.18.255.25:8011';
+export const baseUrl = 'http://172.18.255.251:8588';
 
 // export const baseUrl = 'http://172.18.255.251:18080';
 // export const baseUrl = 'http://172.18.255.251:8588'; // 上传地址
