f85f0ed539daccec12eeb7991145db57f256ddc3
撸撸 <lulu@luludeMacBook-Air.local>
2019-09-02 14:01:17 +0800

    -fix 第一次修改密码提示,登录错误提示修改,机构管理调整

--- src/views/organization/user-list/index.jsx
+++ src/views/organization/user-list/index.jsx
@@ -170,7 +170,6 @@ export default class BasicTable extends React.Component {
 	}
 
 	onKeyup = (e) => {
-		console.log(e.target.value);
 		const { role } = this.state;
 		const { value } = e.target;
 		this.setState({
