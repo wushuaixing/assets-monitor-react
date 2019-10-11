1a3980db13c4b5ddd8640dd95c56e8c84c305b3b
撸撸 <lulu@luludeMacBook-Air.local>
2019-10-11 11:07:06 +0800

    -fix 去掉table内下划线，修复行内hover变色

--- src/views/search/search-data/auction.jsx
+++ src/views/search/search-data/auction.jsx
@@ -50,7 +50,7 @@ class AUCTION extends React.PureComponent {
 		const fildes = getFieldsValue();
 		fildes.startTime = startTime;
 		fildes.endTime = endTime;
-		console.log(fildes);
+		// console.log(fildes);
 
 		if (fildes.lowestConsultPrice && Number(fildes.lowestConsultPrice) > fildes.highestConsultPrice && Number(fildes.highestConsultPrice)) {
 			message.error('评估价格最低价不能高于评估价格最高价！');
@@ -58,6 +58,8 @@ class AUCTION extends React.PureComponent {
 		}
 		// 判断是否为空对象,非空请求接口
 		if (!objectKeyIsEmpty(fildes)) {
+			// console.log(fildes);
+
 			// 将值传到URL
 			navigate(generateUrlWithParams('/search/detail/auction', fildes));
 		} else {
