46c0203d420463a834725ae824eaa57fe1fffa91
吴文怡 <wuwy@yczcjk.com>
2019-10-30 09:22:23 +0800

    -fix: eCharts.min临时

--- src/views/portrait-inquiry/stock-right/index.jsx
+++ src/views/portrait-inquiry/stock-right/index.jsx
@@ -1,67 +1,107 @@
 import React from 'react';
 import './style.scss';
-
-
-const dataSource = {
-	name: '总公司',
-	upper: [
-		{
-			name: '股东1',
-			people: '老王',
-			upper: [
-				{ name: 'info1' },
-				{ name: 'info2' },
-				{ name: 'info3' },
-			],
-		},
-		{
-			name: '股东2',
-			people: '老李',
-			upper: [
-				{ name: 'info11' },
-				{ name: 'info22' },
-			],
-		},
-	],
-	lower: [
-		{
-			name: '分公司1',
-			people: '老吴',
-			lower: [
-				{ name: 'info3' },
-				{ name: 'info4' },
-			],
-		},
-		{
-			name: '分公司2',
-			people: '老赵',
-			lower: [
-				{ name: 'info 7' },
-				{ name: 'info 6' },
-				{ name: 'info 8' },
-				{ name: 'info 9' },
-			],
-		},
-	],
-};
-
+import data from './data';
 
 export default class StockRight extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			DataOwn: [data],
+		};
 	}
 
 	componentDidMount() {
-		// this.drawChart();
-		window.drawTree(dataSource);
+		const { DataOwn } = this.state;
+		const myChart = window.echarts.init(document.getElementById('main'));
+		myChart.setOption({
+
+			tooltip: {
+				trigger: 'item',
+				triggerOn: 'mousemove',
+			},
+
+			series: [
+				{
+					type: 'tree',
+					data: DataOwn,
+					left: '2%',
+					right: '2%',
+					top: '20%',
+					bottom: '8%',
+
+					symbol: 'rectangle',
+					symbolSize: [60, 30],
+
+					orient: 'BT',
+
+					expandAndCollapse: true,
+
+
+					label: {
+						/* formatter: '{b|{b}}', */
+						/*formatter: function(params) {
+
+							//console.log(params.name, params.data.children.length,params.data)
+							let dataName='{a|'+ params.data.name +'}';
+							/!*if(params.data.children.length >1){
+								for(let i=0; i<params.data.children.length; i++){
+									console.log(params.data.children[i].children)
+									dataName += '{a|'+ params.data.children[i].name +'}{b|'+ params.data.children[i].children +'}';
+									console.log(dataName)
+								}
+							}
+							else{*!/
+								dataName +='{b|'+ params.data.name +'}';
+							//}
+
+							//
+							return dataName;
+						},
+						rich: {
+							a: {
+
+								verticalAlign:'bottom',
+								backgroundColor: 'black',
+							},
+							b: {
+								lineHeight: 30,
+								verticalAlign:'top',
+								borderWidth:4,
+								backgroundColor: 'red',
+
+							}
+						},*/
+						position: 'inside',
+						rotate: 0,
+						verticalAlign: 'middle',
+					},
+
+					leaves: {
+						label: {
+							position: 'inside',
+							rotate: 0,
+							verticalAlign: 'middle',
+						},
+					},
+					itemStyle: {
+						color: '#f6faff',
+						borderColor: '#1e81e1',
+					},
+
+					lineStyle: {
+						color: 'steelblue',
+						width: '1.5',
+						/* symbol: 'arrow', */
+					},
+					animationDurationUpdate: 750,
+				},
+			],
+		});
 	}
 
 	render() {
 		return (
-			<div className="container" id="d3_container">
-				<div className="basic" id="tree" />
-			</div>
+			<div className="container" id="main" style={{ width: 1000, height: 500 }} />
 		);
 	}
 }
