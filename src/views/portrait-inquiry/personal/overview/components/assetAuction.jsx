a0c58cd27e8601cd3820bbc3e628be05e73ab270
撸撸 <lulu@luludeMacBook-Air.local>
2019-10-30 17:47:02 +0800

    -feat 添加按钮切换

--- src/views/portrait-inquiry/personal/overview/components/assetAuction.jsx
+++ src/views/portrait-inquiry/personal/overview/components/assetAuction.jsx
@@ -6,9 +6,9 @@ export default class OverView extends React.Component {
 		super(props);
 		this.state = {
 			columnarData: [
-				{ value: 18, name: '资产所有人' },
+				{ value: 8, name: '资产所有人' },
 				{ value: 12, name: '债权人' },
-				{ value: 10, name: '竞买人' },
+				{ value: 9, name: '竞买人' },
 				{ value: 9, name: '资产线索' },
 			],
 			RingData: [
@@ -16,10 +16,45 @@ export default class OverView extends React.Component {
 				{ value: 14, name: '正在进行' },
 				{ value: 10, name: '即将开始' },
 				{ value: 5, name: '已流拍' },
-				{ value: 7, name: '中止' },
-				{ value: 6, name: '撤回' },
 			],
 		};
+	}
+
+	checkTime = (time) => {
+		if (time === 'threeMonth') {
+			this.setState({
+				columnarData: [
+					{ value: 8, name: '资产所有人' },
+					{ value: 12, name: '债权人' },
+					{ value: 9, name: '竞买人' },
+					{ value: 9, name: '资产线索' },
+				],
+				RingData: [
+					{ value: 20, name: '已成交' },
+					{ value: 14, name: '正在进行' },
+					{ value: 10, name: '即将开始' },
+					{ value: 5, name: '已流拍' },
+				],
+			});
+		} else if (time === 'all') {
+			this.setState({
+				columnarData: [
+					{ value: 18, name: '资产所有人' },
+					{ value: 12, name: '债权人' },
+					{ value: 10, name: '竞买人' },
+					{ value: 9, name: '资产线索' },
+					{ value: 19, name: '股权分布' },
+				],
+				RingData: [
+					{ value: 20, name: '已成交' },
+					{ value: 14, name: '正在进行' },
+					{ value: 10, name: '即将开始' },
+					{ value: 5, name: '已流拍' },
+					{ value: 7, name: '中止' },
+					{ value: 6, name: '撤回' },
+				],
+			});
+		}
 	}
 
 	render() {
@@ -33,8 +68,8 @@ export default class OverView extends React.Component {
 				</div>
 				<div className="overview-container-content">
 					<div style={{ marginBottom: 20 }}>
-						<Button>三个月内 8</Button>
-						<Button>全部 12</Button>
+						<Button onClick={() => this.checkTime('threeMonth')}>三个月内 8</Button>
+						<Button onClick={() => this.checkTime('all')}>全部 12</Button>
 					</div>
 					<ColumnarEcharts title="角色分布" Data={columnarData} id="assetAuction" />
 					<RingEcharts title="拍卖结果分布" Data={RingData} id="assetAuction" />
