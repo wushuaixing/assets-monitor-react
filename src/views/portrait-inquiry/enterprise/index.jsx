c6723aee4b343b427313eef3c1aa6bb436fd9662
metotna <me-totna@foxmail.com>
2019-11-01 11:36:34 +0800

    -feat 【画像查询-企业】企业基本信息

--- src/views/portrait-inquiry/enterprise/index.jsx
+++ src/views/portrait-inquiry/enterprise/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Router from '@/utils/Router';
 import QueryView from '../common/queryView';
-import { Tabs } from '@/common';
+import { Tabs, Button } from '@/common';
 import Overview from './overview';
 import Assets from './assets';
 import Lawsuits from './lawsuits';
 import Manage from './manage';
 import Info from './info';
-import { Affix } from 'antd';
+import { Affix, Icon } from 'antd';
 import { navigate } from '@reach/router';
-import TempImg from '@/views/portrait-inquiry/title.png';
+import Dishonest from '@/assets/img/icon/icon_shixin.png';
 import './style.scss';
 
 const source = () => [
@@ -44,6 +44,69 @@ const source = () => [
 		field: 'ignoreCount',
 	},
 ];
+/* 企业概要 */
+const EnterpriseInfo = (props) => {
+	const { download } = props;
+	return (
+		<div className="enterprise-info">
+			<div className="intro-icon">
+				<span>浙江天赐</span>
+			</div>
+			<div className="intro-content">
+				<div className="intro-title">
+					<span className="yc-public-title-large-bold intro-title-name">
+						{'浙江天赐生态科技有限公司'}
+						<img className="intro-title-tag" src={Dishonest} alt="" />
+					</span>
+					<span className="intro-title-status">续存</span>
+				</div>
+				<div className="intro-base-info">
+					<li className="intro-info-list">
+						<span className="yc-public-remark">法定代表人：</span>
+						<span className="yc-public-title">井永柱</span>
+					</li>
+					<li className="intro-info-list">
+						<span className="yc-public-remark">注册资本：</span>
+						<span className="yc-public-title">10655.81万元人民币</span>
+					</li>
+					<li className="intro-info-list">
+						<span className="yc-public-remark">成立日期：</span>
+						<span className="yc-public-title">2003-04-29</span>
+					</li>
+				</div>
+				<div className="intro-used">
+					<li className="intro-info-list">
+						<span className="yc-public-remark">曾用名：</span>
+						<span className="yc-public-title">杭州浙大天赐生态科技有限公司</span>
+					</li>
+				</div>
+			</div>
+			<Button className="intro-download" onClick={download}>
+				<Icon type="download" />
+				{'下载'}
+			</Button>
+		</div>
+	);
+};
+/* 企业概要-简单版 */
+const EnterpriseInfoSimple = (props) => {
+	const { download } = props;
+	return (
+		<div className="enterprise-info">
+			<div className="intro-title">
+				<span className="yc-public-title-large-bold intro-title-name">
+					{'浙江天赐生态科技有限公司'}
+					<img className="intro-title-tag" src={Dishonest} alt="" />
+				</span>
+				<span className="intro-title-status">续存</span>
+			</div>
+			<Button className="intro-download" onClick={download}>
+				<Icon type="download" />
+				{'下载'}
+			</Button>
+		</div>
+	);
+};
 
 export default class Enterprise extends React.Component {
 	constructor(props) {
@@ -54,13 +117,23 @@ export default class Enterprise extends React.Component {
 			tabConfig: source(),
 			childDom: '',
 			sourceType: defaultSourceType ? Number(defaultSourceType[0]) : 101,
+			affixStatus: false,
 		};
 	}
+
+	handleDownload=() => {
+		console.log('handleDownload');
+	};
 
 	handleAddChild=(val) => {
 		this.setState({
 			childDom: val,
 		});
+	};
+
+	onChangeAffix=(val) => {
+		this.setState({ affixStatus: val });
+		// console.log('onChangeAffix:', val);
 	};
 
 	onSourceType=(val) => {
@@ -76,15 +149,21 @@ export default class Enterprise extends React.Component {
 	};
 
 	render() {
-		const { tabConfig, childDom, sourceType } = this.state;
+		const {
+			tabConfig, childDom, sourceType, affixStatus,
+		} = this.state;
 		return (
 			<div className="yc-inquiry-enterprise">
 				<QueryView type={1} />
 				<div className="mark-line" />
 				<div className="inquiry-enterprise-content">
-					<Affix>
+					<Affix onChange={this.onChangeAffix}>
 						<div className={`enterprise-intro ${childDom ? '' : 'enterprise-intro-child'}`} id="enterprise-intro">
-							<img src={TempImg} alt="" style={{ width: '100%' }} />
+							{
+								affixStatus
+									? <EnterpriseInfoSimple download={this.handleDownload} />
+									: <EnterpriseInfo download={this.handleDownload} />
+							}
 							<Tabs.Simple
 								onChange={this.onSourceType}
 								source={tabConfig}
