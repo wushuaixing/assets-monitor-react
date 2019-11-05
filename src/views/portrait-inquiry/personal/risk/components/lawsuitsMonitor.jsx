68d49168db2688b3ae6f992a6c4df755168e73e9
撸撸 <lulu@luludeMacBook-Air.local>
2019-11-05 11:52:54 +0800

    -feat [画像查询 个人风险] 列表页面搭建

---
+++ src/views/portrait-inquiry/personal/risk/components/lawsuitsMonitor.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Pagination } from 'antd';
+import { Spin, Table } from '@/common';
+import assets from '@/utils/api/portrait-inquiry/enterprise/assets';
+import { LawsuitsMonitor } from './common';
+
+const { trial } = assets;
+
+
+export default class lawsuitsMonitor extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dataSource: '',
+			current: 1,
+			total: 0,
+			loading: false,
+		};
+	}
+
+	componentWillMount() {
+		this.toGetData();
+	}
+
+	toGetColumns=() => [
+		{
+			dataIndex: 'caseNumber',
+			render: LawsuitsMonitor.LawsuitsDetail,
+		}, {
+			width: 360,
+			render: LawsuitsMonitor.LawsuitsTrialCourt,
+		},
+	];
+
+	// 当前页数变化
+	onPageChange=(val) => {
+		this.toGetData(val);
+	};
+
+	// 查询数据methods
+	toGetData=(page) => {
+		this.setState({ loading: true });
+		trial.list({
+			page: page || 1,
+		}).then((res) => {
+			console.log(res);
+			if (res.code === 200) {
+				this.setState({
+					dataSource: res.data.list,
+					current: res.data.page,
+					total: res.data.total,
+					loading: false,
+				});
+			} else {
+				this.setState({
+					dataSource: '',
+					current: 1,
+					total: 0,
+					loading: false,
+				});
+			}
+		}).catch(() => {
+			this.setState({ loading: false });
+		});
+	};
+
+	render() {
+		const { dataSource, current, total } = this.state;
+		const { loading } = this.state;
+		const { id } = this.props;
+		return (
+			<div className="yc-inquiry-public-table" id={id}>
+				<div className="public-table-tab">
+					<div className="yc-tabs-simple-prefix">
+                        裁判文书
+						<span className="yc-table-num">{total}</span>
+					</div>
+				</div>
+				<div className="yc-assets-auction">
+					<Spin visible={loading}>
+						<Table
+							rowClassName={() => 'yc-assets-auction-table-row'}
+							columns={this.toGetColumns()}
+							dataSource={dataSource}
+							showHeader={false}
+							pagination={false}
+						/>
+						{dataSource && dataSource.length > 0 && (
+							<div className="yc-table-pagination">
+								<Pagination
+									showQuickJumper
+									current={current || 1}
+									total={total || 0}
+									onChange={this.onPageChange}
+									showTotal={totalCount => `共 ${totalCount} 条信息`}
+								/>
+							</div>
+						)}
+					</Spin>
+				</div>
+			</div>
+		);
+	}
+}
--- src/views/portrait-inquiry/personal/risk/index.jsx
+++ src/views/portrait-inquiry/personal/risk/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Button } from '@/common';
+import LawsuitsMonitor from './components/lawsuitsMonitor';
+import BreakFaith from './components/breakFaith';
+import TaxViolation from './components/taxViolation';
 
 const subItems = [
 	{
@@ -7,18 +10,24 @@ const subItems = [
 		name: '涉诉文书',
 		total: 10,
 		disabled: false,
+		tagName: 'e-assets-lawsuitsMonitor',
+		component: LawsuitsMonitor,
 	},
 	{
 		id: 2,
-		name: '失效记录',
+		name: '失信记录',
 		total: 10,
 		disabled: false,
+		tagName: 'e-assets-breakFaith',
+		component: BreakFaith,
 	},
 	{
 		id: 3,
 		name: '税收违法',
 		total: 10,
 		disabled: false,
+		tagName: 'e-assets-taxViolation',
+		component: TaxViolation,
 	},
 ];
 export default class Risk extends React.Component {
@@ -32,16 +41,20 @@ export default class Risk extends React.Component {
 		toPushChild(this.toGetSubItems());
 	}
 
+	handleScroll=(eleID) => {
+		const dom = document.getElementById(eleID);
+		const _height = document.getElementById('personal-intro').clientHeight;
+		if (dom) {
+			window.scrollTo(0, document.getElementById(eleID).offsetTop - _height);
+		}
+	};
+
 	toGetSubItems=() => (
 		<div className="yc-intro-sub-items">
 			{
 				subItems.map(item => (
-					<Button
-						className="intro-btn-items"
-						disabled={item.disabled}
-					>
-						{item.name}
-						{item.total ? ` ${item.total}` : ' 0'}
+					<Button className="intro-btn-items" disabled={item.disabled} onClick={() => this.handleScroll(item.tagName)}>
+						{`${item.name}${item.total ? ` ${item.total}` : ' 0'}`}
 					</Button>
 				))
 			}
@@ -51,7 +64,8 @@ export default class Risk extends React.Component {
 	render() {
 		return (
 			<div className="inquiry-assets" style={{ padding: '10px 20px' }}>
-				{'default'}
+				{subItems.map(Item => (
+					Item.total ? <Item.component id={Item.tagName} /> : ''))}
 			</div>
 		);
 	}
