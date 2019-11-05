6d38a401e373f19133589e90d6f024dde3824ee8
metotna <me-totna@foxmail.com>
2019-11-05 17:43:49 +0800

    -fix 【模块】echarts改成 按需引入

--- src/static/require.js
+++ src/static/require.js
@@ -1,9 +1,15 @@
 require.config({
 	paths: {
 		"zrender": "../static/echarts/zrender",
+		"echarts":"../static/echarts",
 	}
 });
-require(["zrender",],function(){
+require([
+	"zrender",
+	"echarts",
+	"echarts/chart/tree",
+],function(zr,echarts){
+	window.echarts=echarts;
 	window.zrDefine={
 		Text:require('zrender/shape/Text')
 	};
