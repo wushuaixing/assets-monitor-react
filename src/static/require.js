2d7b5a94643ad1d9de11ec9bd5fb1279a7168e94
metotna <me-totna@foxmail.com>
2019-11-07 09:30:58 +0800

    -fix 【require.js】格式

--- src/static/require.js
+++ src/static/require.js
@@ -1,19 +1,19 @@
 require.config({
 	paths: {
-		"zrender": "../static/echarts/zrender",
-		"echarts":"../static/echarts",
+		'zrender': '../static/echarts/zrender',
+		'echarts': '../static/echarts',
 	}
 });
 require([
-	"zrender",
-	"echarts",
-	"echarts/chart/tree",
-	"echarts/chart/bar",
-	"echarts/chart/pie",
-],function(zr,echarts){
-	window.echarts=echarts;
-	window.zrDefine={
-		Text:require('zrender/shape/Text'),
-		Circle:require('zrender/shape/Circle'),
+	'zrender',
+	'echarts',
+	'echarts/chart/tree',
+	'echarts/chart/bar',
+	'echarts/chart/pie',
+], function (zr, echarts) {
+	window.echarts = echarts;
+	window.zrDefine = {
+		Text: require('zrender/shape/Text'),
+		Circle: require('zrender/shape/Circle'),
 	};
 });
