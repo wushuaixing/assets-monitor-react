2e9d774a87e3b6dae86a9cda80980048cbcfa4c2
撸撸 <lulu@luludeMacBook-Air.local>
2019-11-11 11:28:59 +0800

    -fix [画像信息] 圆环图调整

--- src/views/portrait-inquiry/common/ringEcharts/index.jsx
+++ src/views/portrait-inquiry/common/ringEcharts/index.jsx
@@ -77,7 +77,6 @@ class RingEcharts extends React.Component {
 		dataList.forEach((item, index) => {
 			const x = base.x + (dataList.length === 4 ? (index > 1 ? 1 : 0) * 60 + (index > 1 ? 1 : 0) * 120 : (index > 2 ? 1 : 0) * 60 + (index > 2 ? 1 : 0) * 120);
 			const y = base.y + (dataList.length === 4 ? 20 * (index > 1 ? index - 2 : index) : 20 * (index > 2 ? index - 3 : index));
-			console.log(dataList.length, x, 11);
 
 			const shapeCircle = new Circle({
 				style: {
