58a4d3a9de0176a8893c2f4ee3a93b5fc2c22518
汪汪 <3286199778@qq.com>
2019-11-29 09:53:32 +0800

    -fix [ie8兼容] 业务视图按钮

--- src/views/portrait-inquiry/common/label-tag/index.jsx
+++ src/views/portrait-inquiry/common/label-tag/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './style.scss';
 
-
 const TagOneSide = params => (
 	<div className={`${params.tag || 'yc-label-tag'} ${params.num === 0 && 'yc-label-disabled'}`} {...params}>
 		<div className="yc-label-tag-content">
