23d6d098bb643055caf237b81bfbe27cb70232fb
汪汪 <3286199778@qq.com>
2019-12-16 19:22:28 +0800

    -fix [登录] 登录页调整

--- src/views/login/header.jsx
+++ src/views/login/header.jsx
@@ -32,6 +32,14 @@ class Login extends React.Component {
 							<Icon className="yc-login-icon" type="info-circle-o" />
 								正式用户都会开通唯一的二级域名，请确认访问正确的二级域名网址进行登录
 						</span>
+						{
+							imgUrl ? '' : (
+								<span className="yc-login-world">
+									<Icon className="yc-login-icon" type="info-circle-o" />
+								此网址非专属地址,签约用户请访问您专属的网址进行登录
+								</span>
+							)
+						}
 					</div>
 				</div>
 			</div>
