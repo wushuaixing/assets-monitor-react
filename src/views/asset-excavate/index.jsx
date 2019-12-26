67554789887ec2a1f66de94aad582a7434041881
汪汪 <3286199778@qq.com>
2019-12-26 10:50:31 +0800

    -fix 全局样式修改

--- src/views/asset-excavate/index.jsx
+++ src/views/asset-excavate/index.jsx
@@ -68,6 +68,7 @@ class MonitorMain extends React.Component {
 			this.onUnReadCount();
 		}, 30 * 1000);
 		const versionUpdate = cookie.get('versionUpdate');
+		// console.log(versionUpdate === 'false');
 		if (versionUpdate === 'true') {
 			this.setState({
 				VersionUpdateModalVisible: true,
