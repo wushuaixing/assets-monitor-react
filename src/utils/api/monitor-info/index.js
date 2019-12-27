fbae9a03376ada311339d32adaa20f21b47dfdbb
async <yuanlb@yczcjk.com>
2019-12-27 18:05:53 +0800

    -fix [接口优化] unReadCount

--- src/utils/api/monitor-info/index.js
+++ src/utils/api/monitor-info/index.js
@@ -3,7 +3,7 @@ import service from '@/utils/service';
 
 // 机构未读数量统计总览
 export const unReadCount = () => {
-	if (global.REQ_STATUS) return false;
+	if (global.REQ_STATUS) return Promise.reject(new Error(null));
 	return service.get('/yc/monitor/count/unreadCount', {})
 		.then(res => res.data);
 };
