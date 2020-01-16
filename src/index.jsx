fac4530e3c282c849e39d913e0c71c1357c70a40
async <yuanlb@yczcjk.com>
2020-01-16 13:50:34 +0800

    -fix [日志更新]

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css';
 import './utils/config';
 
 const Version = 'v4.0.2';
-const BetaNumber = '.9';
+const BetaNumber = '.10';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
