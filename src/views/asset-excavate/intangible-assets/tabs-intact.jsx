6929682c219bac62e9d8f48ec2a25390d0a2b539
wsw <wangsw@yczcjk.com>
2020-02-26 16:24:12 +0800

    fix 无形资产-列表全部已读后，tab红点消失

--- src/views/asset-excavate/intangible-assets/tabs-intact.jsx
+++ src/views/asset-excavate/intangible-assets/tabs-intact.jsx
@@ -16,52 +16,61 @@ export default class TabsIntact extends React.Component {
 	// 	this.toRefreshCount(source, sourceType);
 	// }
 
-toRefreshCount=(config, type) => {
-	const { onRefresh } = this.props;
-	const _source =	config;
-	this.toGetUnReadCount(_source);
-	config.forEach((i, index) => {
-		if (i.id !== type) {
-			API(i.id, 'listCount')({}).then((res) => {
-				if (res.code === 200) {
-					_source[index].number = res.data;
-					this.setState({ _source });
-					if (onRefresh)onRefresh(_source);
-				}
-			});
-		}
-	});
-};
-
-toGetUnReadCount=(config) => {
-	const { onRefresh } = this.props;
-	unReadCount().then((res) => {
-		const { data, code } = res;
-		if (code === 200) {
-			const result = config.map((item) => {
-				const _item = item;
-				if (_item.id === 'YC020701')_item.dot = data.companyAbnormalCount;
-				if (_item.id === 'YC020702')_item.dot = data.changeFlag;
-				if (_item.id === 'YC020703')_item.dot = data.companyIllegalCount;
-				if (_item.id === 'YC020704')_item.dot = data.taxCount;
-				return _item;
-			});
-			this.setState({ _source: result });
-			if (onRefresh)onRefresh(result);
-		}
-	});
-};
-
-render() {
-	const { onChange, field } = this.props;
-	const { _source } = this.state;
-	const params = {
-		source: _source,
-		onChange,
-		field,
+	toRefreshCount=(config, type) => {
+		const { onRefresh } = this.props;
+		const _source =	config;
+		this.toGetUnReadCount(_source);
+		config.forEach((i, index) => {
+			if (i.id !== type) {
+				API(i.id, 'listCount')({}).then((res) => {
+					if (res.code === 200) {
+						_source[index].number = res.data;
+						this.setState({ _source });
+						if (onRefresh)onRefresh(_source);
+					}
+				});
+			}
+		});
 	};
-	return (
-		<Tabs.Simple {...params} />
-	);
-}
+
+	toGetUnReadCount=(config) => {
+		const { onRefresh } = this.props;
+		const result = config.map((item) => {
+			const key = item;
+			API(key.id, 'listCount')({ isRead: 0 }).then((res) => {
+				key.dot = res.data;
+			});
+			return key;
+		});
+		this.setState({ _source: result });
+		if (onRefresh)onRefresh(result);
+		/* unReadCount().then((res) => {
+			const { data, code } = res;
+			if (code === 200) {
+				const result = config.map((item) => {
+					const _item = item;
+					if (_item.id === 'YC020701')_item.dot = data.companyAbnormalCount;
+					if (_item.id === 'YC020702')_item.dot = data.changeFlag;
+					if (_item.id === 'YC020703')_item.dot = data.companyIllegalCount;
+					if (_item.id === 'YC020704')_item.dot = data.taxCount;
+					return _item;
+				});
+				this.setState({ _source: result });
+				if (onRefresh)onRefresh(result);
+			}
+		}); */
+	};
+
+	render() {
+		const { onChange, field } = this.props;
+		const { _source } = this.state;
+		const params = {
+			source: _source,
+			onChange,
+			field,
+		};
+		return (
+			<Tabs.Simple {...params} />
+		);
+	}
 }
