5b6172c0eb1cf8d0aed6c0604ea16eaa68181540
async <yuanlb@yczcjk.com>
2020-02-28 16:25:38 +0800

    -fix [public] loadable loading

--- src/common/loadable/index.jsx
+++ src/common/loadable/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Loadable from 'react-loadable';
+import { Spin } from '@/common';
 
 const loadableMethods = ele => (ele ? Loadable({
 	loader: ele,
-	loading: () => <div style={{ padding: 10 }}>loading</div>,
+	loading: () => <Spin visible text=" " />,
 }) : '');
 
 export default loadableMethods;
