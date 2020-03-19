f8664c3b7c5009d9c5077a26193bda0831f86eb1
async <yuanlb@yczcjk.com>
2020-03-19 14:53:10 +0800

    -fix [] Version：v4.0.3-beta.3

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css';
 import './utils/config';
 
 const Version = 'v4.0.3';
-const BetaNumber = '.2';
+const BetaNumber = '.3';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
