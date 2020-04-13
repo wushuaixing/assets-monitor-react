214719ce0a7d94faa504361efe81a724d4ea7f23
async <yuanlb@yczcjk.com>
2020-04-13 14:58:26 +0800

    -fix [detail-business] add dishonest detail

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css';
 import './utils/config';
 
 const Version = 'v4.2.0';
-const BetaNumber = '';
+const BetaNumber = '.2';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
