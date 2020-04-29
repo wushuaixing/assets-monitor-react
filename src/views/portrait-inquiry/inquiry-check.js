1ae417fbd83ca274856319e613836405977e4d3a
async <yuanlb@yczcjk.com>
2020-04-29 15:16:04 +0800

    -fix [inquiry] 【ID1005399】【画像查询次数】个人查询时，机构没有限制画像查询次数，却弹出了画像查询次数不足

--- src/views/portrait-inquiry/inquiry-check.js
+++ src/views/portrait-inquiry/inquiry-check.js
@@ -47,39 +47,42 @@ export const inquiryCheck = (url, type, remind = true) => {
 };
 
 export const noneRemind = affirm => inquiryLimit().then((res) => {
-	const { portraitLimitCount, portraitLimitUseCount } = res.data;
-	const degree = portraitLimitCount - portraitLimitUseCount;
-	// eslint-disable-next-line no-shadow
-	return new Promise((resolve) => {
-		if (affirm) {
-			if (degree > 0) {
-				Modal.confirm({
-					title: '刷新后将重新获取债务人画像！',
-					content: contentStrRemind(degree),
-					iconType: 'exclamation-circle',
-					className: 'message-confirm-icon',
-					onOk() {
-						resolve();
-					},
-					onCancel() {
-						navigate('/inquiry');
-						// eslint-disable-next-line prefer-promise-reject-errors
-					},
-				});
+	if (global.PORTRAIT_INQUIRY_ALLOW) {
+		const { portraitLimitCount, portraitLimitUseCount } = res.data;
+		const degree = portraitLimitCount - portraitLimitUseCount;
+		// eslint-disable-next-line no-shadow
+		return new Promise((resolve) => {
+			if (affirm) {
+				if (degree > 0) {
+					Modal.confirm({
+						title: '刷新后将重新获取债务人画像！',
+						content: contentStrRemind(degree),
+						iconType: 'exclamation-circle',
+						className: 'message-confirm-icon',
+						onOk() {
+							resolve();
+						},
+						onCancel() {
+							navigate('/inquiry');
+							// eslint-disable-next-line prefer-promise-reject-errors
+						},
+					});
+				} else {
+					Modal.warning({
+						title: '画像查询次数已用完',
+						content: '若需获取更多查询次数请联系销售或客服！',
+						iconType: 'exclamation-circle',
+						className: 'message-confirm-icon',
+						onOk() {
+							navigate('/inquiry');
+							// eslint-disable-next-line prefer-promise-reject-errors
+						},
+					});
+				}
 			} else {
-				Modal.warning({
-					title: '画像查询次数已用完',
-					content: '若需获取更多查询次数请联系销售或客服！',
-					iconType: 'exclamation-circle',
-					className: 'message-confirm-icon',
-					onOk() {
-						navigate('/inquiry');
-						// eslint-disable-next-line prefer-promise-reject-errors
-					},
-				});
+				resolve();
 			}
-		} else {
-			resolve();
-		}
-	});
+		});
+	}
+	return new Promise(resolve => resolve());
 });
