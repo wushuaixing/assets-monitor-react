4c383837788d6ef33a5ce68b8e0b54c7979754fe
async <yuanlb@yczcjk.com>
2020-05-07 18:17:30 +0800

    -fix [stock] params : companyId => id && api change

--- src/views/portrait-inquiry/stock-right/index.jsx
+++ src/views/portrait-inquiry/stock-right/index.jsx
@@ -321,6 +321,7 @@ export default class StockRight extends React.Component {
 	// 处理点击后数据
 	handleOption=(params) => {
 		const { id, iconStatus, treeName } = params;
+		const { field, isBusiness } = this.props;
 		let idItem = this.toGetIdItem(id, treeName);
 		// console.log(idItem);
 		if (typeof idItem === 'object') {
@@ -336,7 +337,9 @@ export default class StockRight extends React.Component {
 					idItem.children = idItem.backup;
 					idItem.backup = [];
 				} else {
-					stockChart({ id })
+					const _field = field || 'companyId';
+					const api = isBusiness ? businessStockChart : stockChart;
+					api({ [_field]: id })
 						.then((res) => {
 							if (res.code === 200) {
 								if (treeName === 'investor') {
