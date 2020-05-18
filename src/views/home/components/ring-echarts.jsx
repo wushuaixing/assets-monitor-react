ad1624e65457590755faaf84b732c1fb7748188c
汪汪 <3286199778@qq.com>
2020-05-18 18:06:23 +0800

    - fix 可视化图表兼容ie8

--- src/views/home/components/ring-echarts.jsx
+++ src/views/home/components/ring-echarts.jsx
@@ -99,6 +99,72 @@ const getOption = (Data, id, title, newRingArray, customColorArray) => ({
 	],
 });
 
+const getIeOption = (Data, id, title, newRingArray, customColorArray) => ({
+	tooltip: {
+		trigger: 'item',
+		formatter: '{a} <br/>{b} : {c} ({d}%)',
+	},
+	color: customColorArray || ['#1C80E1', '#45A1FF', '#59C874', '#FCD44A', '#FB8E3C', '#F2657A', '#965EE3', '#4561FF'],
+	legend: {
+		selectedMode: false, // 取消图例上的点击事件
+		orient: 'vertical',
+		// 水平对齐方式，可设置为'left','center','right',number(px)
+		x: '150px',
+		// 垂直对齐方式，可设置为'top','center','bottom',number(px)
+		y: 'center',
+		itemGap: 5,
+		// 距顶部的距离，其他同理
+		zlevel: 3,
+		data: newRingArray,
+		formatter: (name) => {
+			let res = '';
+			for (let i = 0; i < newRingArray.length; i += 1) {
+				if (newRingArray[i].name === name) {
+					res = newRingArray[i].count;
+				}
+			}
+			const arr = [
+				`${name}  `,
+				`${res} 条`,
+			];
+			return arr.join('');
+		},
+	},
+	series: [
+		{
+			name: title,
+			type: 'pie',
+			radius: ['45%', '70%'],
+			center: ['15%', '50%'],
+			label: {
+				normal: {
+					formatter: '{b}\n  {c}',
+				},
+			},
+			itemStyle: {
+				normal: {
+					label: {
+						show: false,
+					},
+					labelLine: {
+						show: false,
+					},
+				},
+				emphasis: {
+					label: {
+						show: true,
+						position: 'center',
+						textStyle: {
+							fontSize: '14',
+							fontWeight: 'bold',
+						},
+					},
+				},
+			},
+			data: newRingArray,
+		},
+	],
+});
 class RingEcharts extends PureComponent {
 	constructor(props) {
 		super(props);
@@ -121,6 +187,8 @@ class RingEcharts extends PureComponent {
 		const {
 			id, title, customColorArray, Data, getRingEchartsType,
 		} = this.props;
+		// 判断是否是ie8
+		const isIe = document.documentMode === 8;
 		// 添加需要的字段名称
 		const newRingArray = [];
 		if (Data) {
@@ -130,7 +198,7 @@ class RingEcharts extends PureComponent {
 				));
 		}
 		const DOM = document.getElementById(`${id}RingEcharts`);
-		const myChart = echarts.init(DOM);
+		const myChart = isIe ? window.echarts.init(DOM) : echarts.init(DOM);
 		myChart.on('legendSelectChanged', (obj) => {
 			const { selected, name } = obj;
 			// const legend = obj.name; // 使用 legendToggleSelect Action 会重新触发 legendSelectChanged Event，导致本函数重复运行 // 使得 无 selected 对象
@@ -138,8 +206,72 @@ class RingEcharts extends PureComponent {
 				getRingEchartsType(obj);
 			}
 		});
-		const option = getOption(Data, id, title, newRingArray, customColorArray);
 
+		const option = isIe ? getIeOption(Data, id, title, newRingArray, customColorArray) : getOption(Data, id, title, newRingArray, customColorArray);
+
+		const { color, series: { 0: { data: dataList } } } = option;
+		if (isIe) {
+			delete option.legend;
+			const { Text, Circle } = window.zrDefine;
+			const zr = myChart.getZrender();
+			const base = {
+				x: 200,
+				y: 40,
+			};
+			dataList.forEach((item, index) => {
+				const x = base.x + (dataList.length === 4 ? (index > 1 ? 1 : 0) * 60 + (index > 1 ? 1 : 0) * 120 : (index > 3 ? 1 : 0) * 60 + (index > 3 ? 1 : 0) * 120);
+				const y = base.y + (dataList.length === 4 ? 25 * (index > 1 ? index - 2 : index) : 25 * (index > 3 ? index - 4 : index));
+
+				const shapeCircle = new Circle({
+					style: {
+						x,
+						y,
+						r: 2,
+						brushType: 'both',
+						color: color[index],
+						strokeColor: color[index],
+						lineWidth: 3,
+					},
+				});
+				const text1 = new Text({
+					style: {
+						x: x + 10,
+						y: y + 2,
+						text: item.typeName || item.type,
+						textFont: 'normal 12px verdana',
+						textAlign: 'left',
+						color: '#333',
+					},
+				});
+				text1.hoverable = false;
+				const text2 = new Text({
+					style: {
+						x: x + 110,
+						y: y + 2,
+						text: item.count,
+						textFont: 'bold 12px Arial',
+						textAlign: 'right',
+						color: '#333',
+					},
+				});
+				text2.hoverable = false;
+				const text3 = new Text({
+					style: {
+						x: x + 130,
+						y: y + 2,
+						text: '条',
+						textFont: 'normal 12px verdana',
+						textAlign: 'right',
+						color: '#333',
+					},
+				});
+				text3.hoverable = false;
+				zr.addShape(shapeCircle);
+				zr.addShape(text1);
+				zr.addShape(text2);
+				zr.addShape(text3);
+			});
+		}
 		myChart.setOption(option);
 	};
 
