3bcc5cfff512e2871e48de35b4a85df6c3b8cad1
zhanglulu <zhangll@yczcjk.com>
2020-07-07 19:11:53 +0800

    - feat [views] 版本更新通知的弹窗

--- src/views/_others/layout/versionUpdateModal.jsx
+++ src/views/_others/layout/versionUpdateModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal, Button } from 'antd';
-import VersionUpdate from 'img/icon/updata2.0.png';
+import Visionpdate from 'img/icon/update2.2.png';
+import closeUpdate from 'img/icon/closeUpdate.png';
 import './style.scss';
 
 export default class VersionUpdateModal extends React.PureComponent {
@@ -18,62 +19,49 @@ export default class VersionUpdateModal extends React.PureComponent {
 		const { VersionUpdateModalVisible } = this.props;
 		return (
 			<Modal
-				width={600}
-				style={{ 'max-height': 770 }}
+				width={540}
+				style={{ 'max-height': 770, 'background-color': 'rgba(0, 0, 0, .0)' }}
 				className="yc-versionUpdate"
 				closable={false}
 				visible={VersionUpdateModalVisible}
 				footer={null}
 				// onCancel={this.handleCancel}
 			>
-				<div>
-					<img src={VersionUpdate} alt="" />
+				<div className="yc-title-box">
+					<img className="yc-close" src={closeUpdate} alt="" onClick={this.handleCancel} />
+					<img className="yc-title-img" src={Visionpdate} alt="" />
+				</div>
+				<div className="yc-modal-box">
 					<div className="yc-modal-content">
 						<div className="yc-modal-title">【版本更新通知】</div>
 						<div>
 							<p className="yc-label-title">
 								<span className="yc-modal-icon" />
-								首页重磅升级！
+								分类搜索增加：土地信息、股权质押、动产抵押、失信记录
 							</p>
 							<div className="yc-modal-list">
-								新增数据动态提醒，债务人
-								<span className="yc-modal-list-detail">重要线索更新不遗漏</span>
-							</div>
-							<div className="yc-modal-list">
-								整合各类快速入口，常用功能、数据详情
-								<span className="yc-modal-list-detail">一键直达</span>
+								资产评估、风险识别新抓手
 							</div>
 						</div>
 						<div>
 							<p className="yc-label-title">
 								<span className="yc-modal-icon" />
-								信息监控入口改版，类目总览信息全面整合！
+								顶部导航栏 - 信息监控增加三级导航
 							</p>
 							<div className="yc-modal-list">
-								各类信息总体匹配情况
-								<span className="yc-modal-list-detail">一目了然 </span>
-								，浏览目标更精准
+								信息切换更健康
 							</div>
 						</div>
 						<div>
 							<p className="yc-label-title">
 								<span className="yc-modal-icon" />
-								两类信息搜索功能入口合并
+								业务视图/债务人视图展示资产、风险线索数
 							</p>
 							<div className="yc-modal-list">
-								精简功能类目，减少功能理解和查找负担
+								资产/风险情况一目了然
 							</div>
 						</div>
-						<div>
-							<p className="yc-label-title">
-								<span className="yc-modal-icon" />
-								机构统计指标优化与迁移（迁移至机构管理处）
-							</p>
-							<div className="yc-modal-list">
-								增加阅读率和最近查阅时间指标，更真实地反馈下级数据使用情况
-							</div>
-						</div>
-						<Button className="yc-modal-btn" onClick={this.handleCancel}>立即体验</Button>
+						<Button className="yc-modal-btn" onClick={this.handleCancel}>我知道了</Button>
 					</div>
 				</div>
 			</Modal>
