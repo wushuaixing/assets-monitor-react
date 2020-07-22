9330bbb136d7e6c3f969614b7b1e1caeda82e78a
zhanglulu <zhangll@yczcjk.com>
2020-07-22 13:43:09 +0800

    - feat [监控日报详情] 税收违法table复用

--- src/views/_others/messageDetail/component/illegalTaxation/index.jsx
+++ src/views/_others/messageDetail/component/illegalTaxation/index.jsx
@@ -1,54 +1,6 @@
 import React, { Component } from 'react';
-import { Pagination } from 'antd';
-import { ReadStatus, Attentions } from '@/common/table';
-import { linkDom, timeStandard } from '@/utils';
-import { Table } from '@/common';
-import { illegalTaxationRes } from '../../test';
-import { partyInfo } from '@/views/_common';
-import { followSingle, markRead, unFollowSingle } from '@/utils/api/message';
-
-const columns = onRefresh => [
-	{
-		title: <span style={{ paddingLeft: 11 }}>发布日期</span>,
-		dataIndex: 'publishDate',
-		width: 113,
-		render: (text, record) => ReadStatus(timeStandard(text || '-'), record),
-	}, {
-		title: '当事人',
-		dataIndex: 'parties',
-		width: 300,
-		render: partyInfo,
-	},
-	{
-		title: '案件性质',
-		dataIndex: 'caseNature',
-		render: text => text || '-',
-	}, {
-		title: '源链接',
-		dataIndex: 'url',
-		className: 'tAlignCenter_important',
-		width: 75,
-		render: (text, record) => (record.url ? linkDom(record.url, ' ', '', 'yc-list-link') : '-'),
-	}, {
-		title: global.Table_CreateTime_Text ,
-		dataIndex: 'gmtCreate',
-		width: 90,
-		render: value => timeStandard(value),
-	}, {
-		title: '操作',
-		width: 60,
-		unNormal: true,
-		className: 'tAlignCenter_important',
-		render: (text, row, index) => (
-			<Attentions
-				text={text}
-				row={row}
-				onClick={onRefresh}
-				api={row.isAttention ? unFollowSingle : followSingle}
-				index={index}
-			/>
-		),
-	}];
+import { markRead } from '@/utils/api/message';
+import TableTaxViolation from '@/views/risk-monitor/operation-risk/table/taxViolation';
 
 class IllegalTaxation extends Component {
 	constructor(props) {
@@ -60,7 +12,7 @@ class IllegalTaxation extends Component {
 
 	componentDidMount() {
 		this.setState({
-			dataSource: illegalTaxationRes.data.list,
+			dataSource: [],
 		});
 	}
 
@@ -94,6 +46,14 @@ class IllegalTaxation extends Component {
 			id, title, total,
 		} = this.props;
 		const { dataSource } = this.state;
+		const tableProps = {
+			noSort: true,
+			dataSource,
+			onRefresh: this.onRefresh,
+			onPageChange: this.onPageChange,
+			maxLength: 5,
+			total,
+		};
 		return (
 			<React.Fragment>
 				<div className="messageDetail-table-title" id={id}>
@@ -102,21 +62,7 @@ class IllegalTaxation extends Component {
 				</div>
 				<div className="messageDetail-table-headerLine" />
 				<div className="messageDetail-table-container">
-					<Table
-						rowKey={record => record.id}
-						columns={columns(this.onRefresh)}
-						dataSource={dataSource}
-						pagination={false}
-						rowClassName={record => (record.isRead ? '' : 'yc-row-bold cursor-pointer')}
-						onRowClick={this.toRowClick}
-					/>
-					{dataSource && dataSource.length > 5 && (
-						<div className="yc-table-pagination">
-							<Pagination
-								showQuickJumper
-							/>
-						</div>
-					)}
+					<TableTaxViolation {...tableProps} />
 				</div>
 			</React.Fragment>
 		);
