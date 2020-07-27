d9c240904b182d1920d02952845db564554bf278
zhanglulu <zhangll@yczcjk.com>
2020-07-27 14:32:48 +0800

    - feat [监控日报详情] list + count 的 message Api

--- src/utils/api/message/message.js
+++ src/utils/api/message/message.js
@@ -1,148 +1,141 @@
+/*
+* 这个数组里面的count 类型的接口没有调用，因为之前已经把各类的信息返回给我了
+* 但是后端提供了这个接口，是可以使用的。
+* */
 import service from '@/utils/service';
 
 const message = [
 	{
 		dataType: 10101,
-		list: params => service.get('/yc/search/portrait/company/asset/auction/precision/list', { params }).then(res => res.data),
+		name: '资产拍卖',
+		list: params => service.get('/yc/report/daily/auction/auctionList', { params }).then(res => res.data),
 		count: (params, id) => service.get('/yc/report/daily/auction/count', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 	{
 		dataType: 10201,
 		name: '代位权-立案  ',
-		params: { partiesName: '国网浙江省电力公司宁波供电公司' },
-		list: params => service.get('/yc/search/portrait/company/asset/subrogation/trial/list', { params }).then(res => res.data),
-		count: (params, id) => service.get('yc/search/portrait/company/asset/subrogation/trial/list-count', { params })
+		list: params => service.get('/yc/report/daily/trial/trialLawsuitList', { params }).then(res => res.data),
+		count: (params, id) => service.get('/yc/report/daily/trial/trialLawsuitCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'subrogation' })),
 	},
 	{
 		dataType: 10202,
 		name: '代位权-开庭',
-		params: { partiesName: '椰树集团有限公司' },
-		list: params => service.get('/yc/search/portrait/company/asset/subrogation/court-notice/list', { params })
+		list: params => service.get('/yc/report/daily/courtNotice/courtNoticeLawsuitList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('yc/search/portrait/company/asset/subrogation/court-notice/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/courtNotice/courtNoticeLawsuitCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'openCourt' })),
 	},
 	{
 		dataType: 10203,
 		name: '代位权-文书',
-		params: { partiesName: '中国民生银行股份有限公司', caseNumber: '苏' },
-		list: params => service.get('/yc/search/portrait/company/asset/subrogation/judgment-document/list', { params })
+		list: params => service.get('/yc/report/daily/judgement/judgementLawsuitList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('yc/search/portrait/company/asset/subrogation/judgment-document/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/judgement/judgementLawsuitCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'instrument' })),
 	},
 	{
 		dataType: 10301,
 		name: '土地数据-土地出让',
-		list: params => service.get('/yc/search/portrait/company/asset/land/transfer', { params })
+		list: params => service.get('/yc/report/daily/landTransfer/landTransferList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/land/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/landTransfer/landTransferCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'transfer' })),
 	},
 	{
 		dataType: 10302,
 		name: '土地数据-土地转让',
-		list: params => service.get('/yc/search/portrait/company/asset/land/transaction', { params })
+		list: params => service.get('/yc/report/daily/landTransaction/landTransactionList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/land/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/landTransaction/landTransactionCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'transaction' })),
 	},
 	{
 		dataType: 10303,
 		name: '土地数据-土地抵押',
-		list: params => service.get('/yc/search/portrait/company/asset/land/mortgage', { params })
+		list: params => service.get('/yc/report/daily/landMortgage/landMortgageList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/land/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/landMortgage/landMortgageCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'mortgage' })),
 	},
 	{
 		dataType: 10401,
 		name: '招标中标',
-		params: { obligorName: '上海浦东物流云计算有限公司', role: 0 },
-		list: params => service.get('/yc/search/portrait/company/asset/stock/pledgor', { params })
+		list: params => service.get('/yc/report/daily/bidding/biddingList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/stock/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/bidding/biddingCount', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 	{
 		dataType: 10501,
 		name: '股权质押',
-		params: { obligorName: '投资', role: 1 },
-		list: params => service.get('/yc/search/portrait/company/asset/stock/pledgee', { params })
+		list: params => service.get('/yc/report/daily/stockPledge/stockPledgeList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/stock/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/stockPledge/stockPledgeCount', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 	{
 		dataType: 10601,
 		name: '金融资产-竞价项目',
-		params: { obligorName: '沈阳铠龙兴业锻铸有限公司', role: 0, regDateStart: '2019-04-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/mortgage/owner', { params })
+		list: params => service.get('/yc/report/daily/auctionBidding/auctionBiddingList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/auctionBidding/auctionBiddingCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'competition' })),
 	},
 	{
 		dataType: 10602,
 		name: '金融资产-公示项目',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/mortgage/people', { params })
+		list: params => service.get('/yc/report/daily/finance/financeList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/finance/financeCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'public' })),
 	},
 	{
 		dataType: 10701,
 		name: '动产抵押',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/emission/list', { params })
+		list: params => service.get('/yc/report/daily/mortgage/mortgageList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/stock/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/mortgage/mortgageCount', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 	{
 		dataType: 10801,
 		name: '无形资产-排污权',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/mining/list', { params })
+		list: params => service.get('/yc/report/daily/emission/emissionList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/emission/emissionCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'pollution' })),
 	},
 	{
 		dataType: 10802,
 		name: '无形资产-矿业权',
 		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/mining/list', { params })
+		list: params => service.get('/yc/report/daily/mining/miningList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/mining/miningCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'mining' })),
 	},
 	{
 		dataType: 10803,
 		name: '无形资产-商标专利',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/trademark/list', { params })
+		list: params => service.get('/yc/report/daily/trademarkRight/trademarkRightList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/trademarkRight/trademarkRightCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'trademark' })),
 	},
 	{
 		dataType: 10804,
 		name: '无形资产-建筑建造资质',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/construct/constructList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/construct/constructCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'building' })),
 	},
 	{
 		dataType: 10901,
 		name: '涉诉监控-立案',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/trial/trialLawsuitList', { params, sourceType: 2 })
 			.then(res => res.data),
 		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
 			.then(res => Object.assign(res.data, { id, field: 'case' })),
@@ -150,55 +143,49 @@ const message = [
 	{
 		dataType: 10902,
 		name: '涉诉监控-开庭',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/courtNotice/courtNoticeLawsuitList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/courtNotice/courtNoticeLawsuitCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'court' })),
 	},
 	{
 		dataType: 10903,
 		name: '涉诉监控-文书',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/judgement/judgementLawsuitList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/judgement/judgementLawsuitCount', { params })
 			.then(res => Object.assign(res.data, { id, field: 'judgmentDocument' })),
 	},
 	{
 		dataType: 11001,
 		name: '企业破产重组',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/bankruptcy/bankruptcyList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/bankruptcy/bankruptcyCount', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 	{
 		dataType: 11101,
 		name: '失信记录',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/dishonest/dishonestList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/dishonest/dishonestCount', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 	{
 		dataType: 11201,
 		name: '税收违法',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/tax/taxList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/tax/taxCount', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 	{
 		dataType: 11202,
 		name: '环保处罚',
-		params: { role: 1, regDateStart: '2019-05-01' },
-		list: params => service.get('/yc/search/portrait/company/asset/intangible/construct/list', { params })
+		list: params => service.get('/yc/report/daily/epb/epbList', { params })
 			.then(res => res.data),
-		count: (params, id) => service.get('/yc/search/portrait/company/asset/mortgage/list-count', { params })
+		count: (params, id) => service.get('/yc/report/daily/epb/epbCount', { params })
 			.then(res => Object.assign(res.data, { id })),
 	},
 ];
