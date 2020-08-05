2a2993dd1241f452d47d408374ac04bd9d3f5d59
zhanglulu <zhangll@yczcjk.com>
2020-08-05 18:54:00 +0800

    - fix [login] 验证码登录

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css';
 import './utils/config';
 
 const Version = 'v2.3.0';
-const BetaNumber = '.28';
+const BetaNumber = '.29';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
