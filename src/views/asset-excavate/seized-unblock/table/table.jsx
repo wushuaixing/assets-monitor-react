23f393c39ab1ee2f7680243d834f94bbdb2c7a72
zhanglulu <zhangll@yczcjk.com>
2020-10-21 15:53:25 +0800

    - fix [ID1009787] UI调整

--- src/views/asset-excavate/seized-unblock/table/table.jsx
+++ src/views/asset-excavate/seized-unblock/table/table.jsx
@@ -30,7 +30,7 @@ const columns = (props) => {
 					text.map(i => (
 						<div style={{ position: 'relative', paddingLeft: 10 }}>
 							<Ellipsis
-								content={`${i.name}${text.length > 1 ? ';' : ''}`}
+								content={`${i.name}`}
 								tooltip
 								width={240}
 								url={`${i.obligorId !== 0 ? `/#/business/debtor/detail?id=${i.obligorId}` : ''}`}
