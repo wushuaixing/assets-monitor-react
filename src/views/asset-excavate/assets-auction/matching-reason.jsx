7391410d3507c176e23fe04710943dc32c8cf7ee
zhanglulu <zhangll@yczcjk.com>
2020-10-26 15:53:11 +0800

    - fix [查解封资产] table调整

--- src/views/asset-excavate/assets-auction/matching-reason.jsx
+++ src/views/asset-excavate/assets-auction/matching-reason.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Icon } from 'antd';
+import './style.scss';
 
 export default class MatchingReason extends React.Component {
 	constructor(props) {
@@ -116,13 +117,13 @@ export default class MatchingReason extends React.Component {
 								<li className="action-ellipsis yc-text-normal">
 									<Icon type="ellipsis" />
 								</li>
-								<li className="action-btn yc-text-normal" onClick={() => this.setState({ status: 'canClose' })}>
+								<li className="action-btn yc-text-normal under-line" onClick={() => this.setState({ status: 'canClose' })}>
 									<span>展开</span>
 									<Icon type="down" />
 								</li>
 							</React.Fragment>
 						) : (
-							<li className="action-btn yc-text-normal" onClick={() => this.setState({ status: 'canOpen' })}>
+							<li className="action-btn yc-text-normal under-line" onClick={() => this.setState({ status: 'canOpen' })}>
 								<span>收起</span>
 								<Icon type="up" />
 							</li>
