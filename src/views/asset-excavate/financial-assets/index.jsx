32d6a977ef96479f0ee853140f0a75af8ffacef4
wsw <wangsw@yczcjk.com>
2020-11-19 09:39:55 +0800

    fix 【【首页带条件跳转】资产拍卖点击跳转时没带条件】     https://www.tapd.cn/32583197/bugtrace/bugs/view/1132583197001009886     【【首页带条件跳转】动产抵押、金融资产-竞价项目、税收违法 跳转时带了条件，但是列表显示的是全部数据】     https://www.tapd.cn/32583197/bugtrace/bugs/view/1132583197001009888

--- src/views/asset-excavate/financial-assets/index.jsx
+++ src/views/asset-excavate/financial-assets/index.jsx
@@ -99,7 +99,10 @@ export default class Subrogation extends React.Component {
 		this.setState({
 			sourceType,
 		});
-		this.onQueryChange({}, sourceType);
+		const url = window.location.hash;
+		if (url.indexOf('?') === -1) {
+			this.onQueryChange({}, sourceType);
+		}
 		this.onUnReadCount();
 		// this.setUnReadCount = setInterval(() => {
 		// 	this.onUnReadCount();
@@ -270,7 +273,7 @@ export default class Subrogation extends React.Component {
 		});
 		this.onUnReadCount();
 		this.toClearSortStatus();
-		this.onQueryChange({}, val, 'all', 1);
+		this.onQueryChange(this.isUrlParams(val), val, 'all', 1);
 		this.selectRow = [];
 		window.location.href = changeURLArg(window.location.href, 'class', val);
 	};
