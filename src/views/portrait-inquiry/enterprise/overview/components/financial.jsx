34930a33c198c50001dafd4ef95db2bf305089fb
zhanglulu <zhangll@yczcjk.com>
2020-11-19 17:41:55 +0800

    - feat [inquiry] 企业画像概览完善

--- src/views/portrait-inquiry/enterprise/overview/components/financial.jsx
+++ src/views/portrait-inquiry/enterprise/overview/components/financial.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { businessOverviewFinancial, overviewFinancial } from '@/utils/api/professional-work/overview';
-import RingEcharts from '@/views/portrait-inquiry/common/ringEcharts';
-import TimeLine from '@/views/portrait-inquiry/common/timeLine';
 import TagSide from '@/views/portrait-inquiry/common/checkBtn';
-import getCount from '@/views/portrait-inquiry/common/getCount';
+import { getFinancial } from '@/utils/api/portrait-inquiry/enterprise/overview';
+import RingEcharts from '../../../common/ringEcharts';
+import TimeLine from '../../../common/timeLine';
+import getCount from '../../../common/getCount';
+
 
 const financeProjectTypeMap = new Map([
 	[1, '股权项目'],
@@ -82,14 +83,12 @@ export default class Financial extends React.Component {
 	}
 
 	getData = () => {
-		const {
-			businessId, obligorId, getAssetProfile, portrait,
-		} = this.props;
-		const params = portrait === 'business' ? { businessId, type: 2 } : { obligorId, type: 2 };
-		const api = portrait === 'business' ? businessOverviewFinancial : overviewFinancial;
-		api(params).then((res) => {
+		const { companyId, getAssetProfile } = this.props;
+		const params = {
+			companyId,
+		};
+		getFinancial(params).then((res) => {
 			if (res.code === 200) {
-				// console.log('finance res === ', res.data);
 				const financeAuctionArray = res.data.financeInfos.filter(i => i.type === 1)[0]; // 竞价项目
 				const financeInvestmentArray = res.data.financeInfos.filter(i => i.type === 2)[0]; // 招商项目
 				const financePublicArray = res.data.financeInfos.filter(i => i.type === 3)[0]; // 公示项目
@@ -133,13 +132,14 @@ export default class Financial extends React.Component {
 					financeInvestmentNum,
 					financePublicNum,
 				});
+			} else {
+				// this.setState({ loading: false });
 			}
 		}).catch(() => {
 			// this.setState({ loading: false });
 		});
 	};
 
-	// bidding 竞价  investment 招商 publicity 公示
 	checkTime = (selectType) => {
 		const { financeAuctionArray, financeInvestmentArray, financePublicArray } = this.state;
 		if (selectType === 'bidding') {
@@ -170,11 +170,9 @@ export default class Financial extends React.Component {
 	};
 
 	render() {
-		const { portrait } = this.props;
 		const {
 			RingData, timeLineData, selectType, financeAuctionArray, financeInvestmentArray, financePublicArray, 	financeAuctionNum, financeInvestmentNum, financePublicNum, RingDataNum, timeLineDataNum,
 		} = this.state;
-
 		return (
 			<div>
 				{timeLineDataNum > 0 || RingDataNum > 0 ? (
@@ -189,44 +187,40 @@ export default class Financial extends React.Component {
 							</span>
 						</div>
 						<div className="overview-container-content">
-							{portrait !== 'debtor_personal' && (
-								<div style={{ marginBottom: 20 }}>
-									<TagSide
-										content="竞价项目"
-										num={financeAuctionNum}
-										onClick={() => {
-											if (financeAuctionNum > 0) {
-												this.checkTime('bidding');
-											}
-										}}
-										tag={selectType === 'bidding' ? 'yc-tag-active' : ''}
-									/>
-									<TagSide
-										content="招商项目"
-										num={financeInvestmentNum}
-										onClick={() => {
-											if (financeInvestmentNum > 0) {
-												this.checkTime('investment');
-											}
-										}}
-										tag={selectType === 'investment' ? 'yc-tag-active' : ''}
-									/>
-									<TagSide
-										content="公示项目"
-										num={financePublicNum}
-										onClick={() => {
-											if (financePublicNum > 0) {
-												this.checkTime('publicity');
-											}
-										}}
-										tag={selectType === 'publicity' ? 'yc-tag-active' : ''}
-									/>
-								</div>
-							)}
-							<div style={{ marginBottom: 20 }} />
-							{RingDataNum > 0 && <RingEcharts title="项目状态分布" Data={RingData} id="financeCharts" customColorArray={['#1C80E1', '#45A1FF', '#59C874', '#FCD44A', '#FB8E3C', '#F2657A', '#965EE3', '#4561FF']} />}
+							<div style={{ marginBottom: 20 }}>
+								<TagSide
+									content="竞价项目"
+									num={financeAuctionNum}
+									onClick={() => {
+										if (financeAuctionNum > 0) {
+											this.checkTime('bidding');
+										}
+									}}
+									tag={selectType === 'bidding' ? 'yc-tag-active' : ''}
+								/>
+								<TagSide
+									content="招商项目"
+									num={financeInvestmentNum}
+									onClick={() => {
+										if (financeInvestmentNum > 0) {
+											this.checkTime('investment');
+										}
+									}}
+									tag={selectType === 'investment' ? 'yc-tag-active' : ''}
+								/>
+								<TagSide
+									content="公示项目"
+									num={financePublicNum}
+									onClick={() => {
+										if (financePublicNum > 0) {
+											this.checkTime('publicity');
+										}
+									}}
+									tag={selectType === 'publicity' ? 'yc-tag-active' : ''}
+								/>
+							</div>
+							{RingDataNum > 0 && <RingEcharts title="案件类型分布" Data={RingData} id="financeEcharts" customColorArray={['#1C80E1', '#45A1FF', '#59C874', '#FCD44A', '#FB8E3C', '#F2657A', '#965EE3', '#4561FF']} />}
 							{timeLineDataNum > 0 && <TimeLine title="年份分布" Data={timeLineData} id="financeTimeLine" />}
-
 						</div>
 					</div>
 				) : null }
