4217a924549ebdf996cf684538127c60afe60403
zhanglulu <zhangll@yczcjk.com>
2020-12-07 18:56:54 +0800

    - feat [update] 版本更新弹窗

--- src/views/_others/layout/versionUpdateModal.jsx
+++ src/views/_others/layout/versionUpdateModal.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Modal, Button } from 'antd';
-import Visionpdate from 'img/icon/update2.2.png';
-import closeUpdate from 'img/icon/closeUpdate.png';
+import version from 'img/icon/version.png';
 import './style.scss';
 
 export default class VersionUpdateModal extends React.PureComponent {
@@ -28,24 +27,33 @@ export default class VersionUpdateModal extends React.PureComponent {
 				// onCancel={this.handleCancel}
 			>
 				<div className="yc-title-box">
-					<img className="yc-close" src={closeUpdate} alt="" onClick={this.handleCancel} />
-					<img className="yc-title-img" src={Visionpdate} alt="" />
+					<img className="yc-title-img" src={version} alt="" />
 				</div>
 				<div className="yc-modal-box">
 					<div className="yc-modal-content">
 						<div className="yc-modal-title">【版本更新通知】</div>
 						<div className="yc-label-box">
 							<div className="yc-label-title">
-								<span className="yc-modal-icon" />
-								<span className="yc-label-tips">金融资产增加招商项目信息</span>
+								<div>
+									<span className="yc-modal-icon" />
+									<span className="yc-label-tips">资产挖掘</span>
+								</div>
+								<div className="yc-label-subTips">新增不动产登记、车辆信息</div>
+							</div>
+							<div className="yc-label-title">
+								<div>
+									<span className="yc-modal-icon" />
+									<span className="yc-label-tips">债务人详情和画像详情</span>
+								</div>
+								<div className="yc-label-subTips">增加查解封资产、金融资产、限制高消费</div>
 							</div>
 							<div className="yc-label-title">
 								<span className="yc-modal-icon" />
-								<span className="yc-label-tips">金融资产-公示项目增加AMC处置公告信息</span>
+								<span className="yc-label-tips">债务人详情和画像详情pdf内容完善</span>
 							</div>
 							<div className="yc-label-title">
 								<span className="yc-modal-icon" />
-								<span className="yc-label-tips">画像pdf完善与交互优化</span>
+								<span className="yc-label-tips">部分页面交互优化</span>
 							</div>
 						</div>
 						<Button className="yc-modal-btn" onClick={this.handleCancel}>我知道了</Button>
