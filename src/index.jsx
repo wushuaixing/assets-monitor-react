d3fe11e5b4562c23c4759eeecf81570639f07220
zhanglulu <zhangll@yczcjk.com>
2020-12-29 10:08:17 +0800

    - fix [debtor] 在建工程table-version

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css/index.scss';
 import './utils/config';
 
 const Version = 'v2.7.0';
-const BetaNumber = '.22';
+const BetaNumber = '.23';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
