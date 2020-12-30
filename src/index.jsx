1363e261dc655f7fda934e37a4e4a17803b48d32
zhanglulu <zhangll@yczcjk.com>
2020-12-30 18:16:13 +0800

    - fix [home] 保持资产线索数据一致

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css/index.scss';
 import './utils/config';
 
 const Version = 'v2.7.0';
-const BetaNumber = '.30';
+const BetaNumber = '.31';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
