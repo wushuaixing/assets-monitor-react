d5f1587c5425ce50c228e6e562dee03287f54301
zhanglulu <zhangll@yczcjk.com>
2021-01-05 14:26:04 +0800

    - feat [construct] 文案

--- src/views/asset-excavate/construct-project/table/winbid.jsx
+++ src/views/asset-excavate/construct-project/table/winbid.jsx
@@ -50,8 +50,7 @@ const columns = (props) => {
 			render: text => <span>{text || '-'}</span>,
 		},
 		{
-			title: (noSort ? '中标信息'
-				: <SortVessel field="PUBLISH_TIME" onClick={onSortChange} mark="(发布日期)" {...sort}>中标信息</SortVessel>),
+			title: '中标信息',
 			width: 496,
 			dataIndex: 'title',
 			render: (text, row) => (
