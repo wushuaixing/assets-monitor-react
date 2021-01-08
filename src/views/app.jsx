6b2f9537c5f50a50c518737ac25e17c1985fe059
zhanglulu <zhangll@yczcjk.com>
2021-01-08 16:38:41 +0800

    -feat [icon] 修改icon

--- src/views/app.jsx
+++ src/views/app.jsx
@@ -8,7 +8,7 @@ export default class BaseRouter extends React.PureComponent {
 	componentWillMount() {
 		const domainName = window.location.hostname;
 		const currentName = domainName.split('.')[0];
-		if (['zhongguan', 'zhongguandev'].includes(currentName)) {
+		if (currentName.indexOf('zhongguan') >= 0) {
 			let link = document.querySelector('link[rel~=\'icon\']');
 			if (!link) {
 				link = document.createElement('link');
