6d9acdc3d05a5e0bb443cdf6fa92fdc287ceb740
LAPTOP-URNVMJJ4\86182 <1761459218@qq.com>
2021-04-07 15:47:39 +0800

    fix[修改监控信息测量]更新字段

--- src/views/asset-excavate/car/table.jsx
+++ src/views/asset-excavate/car/table.jsx
@@ -115,7 +115,6 @@ export default class TableView extends React.Component {
 		const {
 			total, current, dataSource, manage, onPageChange, pageSize, isShowPagination = true,
 		} = this.props;
-		console.log(dataSource);
 		const { selectedRowKeys } = this.state;
 		const rowSelection = manage ? {
 			rowSelection: {
