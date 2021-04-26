ea5efce51a51a195231019ac95297106e2191a14
wsx <1761459218@qq.com>
2021-04-26 14:37:25 +0800

    fix[修改版本]

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css/index.scss';
 import './utils/config';
 
 const Version = 'v2.8.1';
-const BetaNumber = '.23';
+const BetaNumber = '.24';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
