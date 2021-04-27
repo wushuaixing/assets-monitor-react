039568e3b1e65bf54da532ee13325b1e57c9009f
wsx <1761459218@qq.com>
2021-04-27 14:26:14 +0800

    fix[金融资产]公示项目修复bug

--- src/views/asset-excavate/financial-assets/table-version/publicity.jsx
+++ src/views/asset-excavate/financial-assets/table-version/publicity.jsx
@@ -40,12 +40,15 @@ class TableIntact extends React.Component {
 			dataIndex: 'title',
 			render: (value, row) => (
 				<div className="assets-info-content">
+					{
+						console.log('=======',row.title,row.title,row)
+					}
 					<li style={{ display: 'inline-block' }}>
 						<span className="li-td-tag">
 							{projectTypeMap.get(row.projectType)}
 						</span>
-						{ toEmpty(row.title)
-							? <Ellipsis content={row.title || `${row.projectName ? row.projectName : row.sourceUrl}`} url={row.sourceUrl} tooltip width={600} font={15} className="yc-public-title-normal-bold" isSourceLink /> : '-' }
+						{ toEmpty(row.title || (row.projectName ? row.projectName : row.sourceUrl))
+							? <Ellipsis content={row.title || (row.projectName ? row.projectName : row.sourceUrl)} url={row.sourceUrl} tooltip width={600} font={15} className="yc-public-title-normal-bold" isSourceLink /> : '-' }
 					</li>
 				</div>
 			),
