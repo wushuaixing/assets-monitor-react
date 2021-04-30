ef20bf57dbdff389e21a9746ccb1d19daba526e5
xiyu <zhangy@yczcjk.com>
2021-04-30 14:12:35 +0800

    -feat 版本更新提示

--- src/views/_others/layout/versionUpdateModal.jsx
+++ src/views/_others/layout/versionUpdateModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, Button } from 'antd';
-import version from 'img/icon/version.png';
+import version from 'img/icon/update2.8.png';
 import './style.scss';
 
 export default class VersionUpdateModal extends React.PureComponent {
@@ -16,6 +16,7 @@ export default class VersionUpdateModal extends React.PureComponent {
 
 	render() {
 		const { VersionUpdateModalVisible } = this.props;
+		const tips = ['优化首页展示内容', '对于不公开的裁判文书，展示其不公开理由', '完善多类数据的展示和跳转逻辑', '交互优化和bug修复'];
 		return (
 			<Modal
 				width={540}
@@ -33,34 +34,16 @@ export default class VersionUpdateModal extends React.PureComponent {
 					<div className="yc-modal-content">
 						<div className="yc-modal-title">【版本更新通知】</div>
 						<div className="yc-label-box">
-							<div className="yc-label-title">
-								<div>
-									<span className="yc-modal-icon" />
-									<span className="yc-label-tips">资产挖掘</span>
-								</div>
-								<div className="yc-label-subTips">新增在建工程信息</div>
-							</div>
-							<div className="yc-label-title">
-								<div>
-									<span className="yc-modal-icon" />
-									<span className="yc-label-tips">债务人详情和画像详情</span>
-								</div>
-								<div className="yc-label-subTips">增加不动产登记、车辆信息、在建工程信息</div>
-							</div>
-							<div className="yc-label-title">
-								<div>
-									<span className="yc-modal-icon" />
-									<span className="yc-label-tips">展示内容调整</span>
-								</div>
-								<div className="yc-label-subTips">调整业务详情和资产挖掘-限制高消费的展示内容</div>
-							</div>
-							<div className="yc-label-title">
-								<div>
-									<span className="yc-modal-icon" />
-									<span className="yc-label-tips">债务人列表</span>
-								</div>
-								<div className="yc-label-subTips">增加异常工商登记状态和限高状态的展示</div>
-							</div>
+							{
+							    tips.map(i => (
+								  <div className="yc-label-title">
+									<div key={i}>
+										<span className="yc-modal-icon" />
+										<span className="yc-label-tips">{i}</span>
+									</div>
+								  </div>
+							    ))
+							  }
 						</div>
 						<Button className="yc-modal-btn" onClick={this.handleCancel}>我知道了</Button>
 					</div>
