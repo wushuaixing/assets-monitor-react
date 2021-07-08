5c2dee0eb35b357de98be7bf93a7283cf39a22a8
wsx <1761459218@qq.com>
2021-07-08 14:51:33 +0800

    fix(修改版本号)

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import './assets/css/index.scss';
 import './utils/config';
 
 const Version = 'v2.10';
-const BetaNumber = '.1';
+const BetaNumber = '.2';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
