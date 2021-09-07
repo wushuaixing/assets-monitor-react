0a161887ec722234bc8853a41a26920424beedab
xiyu <zhangy@yczcjk.com>
2021-09-07 10:17:24 +0800

    'feat(监控日报列表)：监控日报列表接口修改'

--- src/views/risk-monitor/bankruptcy/relation-notice-modal.jsx
+++ src/views/risk-monitor/bankruptcy/relation-notice-modal.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Modal, Timeline } from 'antd';
 import PropTypes from 'reactPropTypes';
 import './style.scss';
+import { Ellipsis } from '@/common';
 // 信息监控/风险监控/破产重组- 公告信息列|关联公告弹窗
 function RelationNoticeModal(props) {
 	const {
 		visible, onCancel, onOk, list = [],
 	} = props;
+	const fn = i => i || '-';
+	console.log(list);
 	return (
 		<Modal
 			wrapClassName="relation-notice-modal"
@@ -25,9 +28,11 @@ function RelationNoticeModal(props) {
 							list.map(i => (
 								<Timeline.Item key={i.id}>
 									<div className="yc-Timeline-item">
-										 <span className="yc-Timeline-item-publish">{i.gmtPublish}</span>
-										 <span className="yc-Timeline-item-title"><a href={i.url} target="_blank" rel="noreferrer">{i.title}</a></span>
-										 <span className="yc-Timeline-item-typeName">{i.typeName}</span>
+										 <span className="yc-Timeline-item-publish">{fn(i.gmtPublish)}</span>
+										 <span className="yc-Timeline-item-title">
+											 	<a href={i.typeName === '裁判文书' ? `#/judgement?sourceId=10986&pid=${i.pid}&title=${i.title}` : i.url} target="_blank" rel="noreferrer">{fn(i.title)}</a>
+										 </span>
+										 <span className="yc-Timeline-item-typeName">{fn(i.typeName)}</span>
 									</div>
 								</Timeline.Item>
 							))
