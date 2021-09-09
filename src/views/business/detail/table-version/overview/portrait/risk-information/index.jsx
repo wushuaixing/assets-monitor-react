b07bf9f11c4b8794d663ea33bd13e8f91cd77a5f
xiyu <zhangy@yczcjk.com>
2021-09-09 18:41:26 +0800

    fix(债务人详情-概览-画像)：卡片对应字段错误

--- src/views/business/detail/table-version/overview/portrait/risk-information/index.jsx
+++ src/views/business/detail/table-version/overview/portrait/risk-information/index.jsx
@@ -141,12 +141,12 @@ export default class RiskInformation extends React.Component {
 
 	// 破产重组
 	getBankruptcyData = (isArray, res) => {
+		console.log(res);
 		if (isArray && res && res.code === 200) {
-			const { gmtCreate } = res.data;
-			const dataSourceNum = res.data.bankruptcy || 0;
+			const { gmtModified, bankruptcyNum } = res.data;
 			const bankruptcyPropsData = {
-				bankruptcyNum: dataSourceNum,
-				gmtCreate,
+				bankruptcyNum,
+				gmtCreate: gmtModified,
 				obligorTotal: res.data.obligorTotal || 0,
 			};
 			this.setState(() => ({
