85dcd047122f2bddd017566f914e41198bedecd8
xiyu <zhangy@yczcjk.com>
2021-09-14 11:54:05 +0800

    feat(version)：修改版本号v2.12 beta.13

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import './assets/css/index.scss';
 import './utils/config';
 
 const Version = 'v2.12';
-const BetaNumber = '.12';
+const BetaNumber = '.13';
 const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
